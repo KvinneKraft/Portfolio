49b4be2d33e0dba58469609a61d8c765f407960e
KvinneKraft <KvinneKraft@protonmail.com>
2021-01-26 15:24:17 +0200

    creation LogContainer.cs

---
+++ Dashloris-X/Headers/util/LogContainer.cs
@@ -0,0 +1,26 @@
+﻿// Author: Dashie
+// Version: 1.0
+//
+// <description>
+//
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DashlorisX.Headers.util
+{
+    public class LogContainer : Form
+    {
+	readonly DashControls Control = new DashControls();
+	readonly DashTools Tool = new DashTools();
+
+	public LogContainer()
+	{
+
+	}
+    }
+}
