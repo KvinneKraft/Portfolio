22afbfe8276b0b33ee6d53d3dc100be1616e487a
KvinneKraft <KvinneKraft@protonmail.com>
2021-01-28 15:47:18 +0200

    dashbot.cs creation

---
+++ Dashloris-X/Headers/util/DashBox.cs
@@ -0,0 +1,32 @@
+﻿// Author: Dashie
+// Version: 1.0
+//
+// <description>
+//
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace DashlorisX
+{
+    public class DashBox
+    {
+	private class Dialog : Form
+	{
+
+	}
+
+	private static readonly List<Button> buttonList = new List<Button>(); // Initialize Upon First Show
+
+	public static void Show(string message, string title, [Optional] Icon icon, [Optional] Point iconLocation, [Optional] List<Button> buttonList)
+	{
+
+	}
+    }
+}
