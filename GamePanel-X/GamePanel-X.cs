2600f0499ae537f77611b0aaeceabcc051bc1e1c
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-01 21:35:32 +0200

    setup the main dialog

--- GamePanel-X/GamePanel-X.cs
+++ GamePanel-X/GamePanel-X.cs
@@ -19,11 +19,79 @@ namespace GamePanelX
     {
 	private readonly DashDialog DashDialog = new DashDialog();
 
+	private void InitializeMainComponent()
+	{
+	    try
+	    {
+		var MenuBarBColor = Color.FromArgb(8, 8, 8);
+		var AppBColor = Color.FromArgb(24, 24, 24);
+		var AppSize = new Size(350, 350);
+
+		DashDialog.JustInitialize(AppSize, string.Format("GamePanel-X  1.0"), AppBColor, MenuBarBColor, MenuBarMinim:true, CloseHideApp:false);
+
+		DashDialog.MenuBar.Close.Click += (s, e) =>
+		{
+		    Application.Exit();
+		};
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+
+
+	private void InitializeMainContainer()
+	{
+	    try
+	    {
+
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+	private readonly PictureBox BottomBarInnerContainer = new PictureBox();
+	private readonly PictureBox BottomBar = new PictureBox();
+
+	private readonly Button Remove = new Button();
+	private readonly Button Games = new Button();
+	private readonly Button Add = new Button();
+
+	private void InitializeBottomMenuBar()
+	{
+	    try
+	    {
+
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
 	public void StartApp()
 	{
+	    try
+	    {
+		InitializeMainComponent();
+		InitializeMainContainer();
+		InitializeBottomMenuBar();
 
+		Application.Run(DashDialog);
+		Environment.Exit(-1);
+	    }
 
-	    Application.Run(DashDialog);
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
 	}
     }
 }
