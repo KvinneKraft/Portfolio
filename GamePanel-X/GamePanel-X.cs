960cbe0b1eecac569f04cff5b72e5b1aa25a4ea6
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-02 19:24:55 +0200

    removed old code

--- GamePanel-X/GamePanel-X.cs
+++ GamePanel-X/GamePanel-X.cs
@@ -44,97 +44,29 @@ namespace GamePanelX
 	    }
 	}
 
-	private void configValidation()
-	{
-	    try
-	    {
-		if (Directory.Exists("gameData"))
-		{
-		    try
-		    {
-			Directory.CreateDirectory("gameData");
-			File.Create(@"gameData\games.config");
-		    }
-
-		    catch
-		    {
-			MessageBox.Show("Unable to create a required directory.\r\n\r\nPlease make sure you have sufficient privileges to be running this application.\r\n\r\nPress OK to close the application.", "Error Occurred", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
-			DashDialog.Close();
-		    }
-		}
-	    }
-
-	    catch (Exception E)
-	    {
-		throw (ErrorHandler.GetException(E));
-	    }
-	}
-
-	private bool doesGameConfigExist()
-	{
-	    try
-	    {
-		return File.Exists(@"gameData\games.config");
-	    }
-
-	    catch (Exception E)
-	    {
-		throw (ErrorHandler.GetException(E));
-	    }
-	}
-
-	private readonly Dictionary<string, List<string>> GameData = new Dictionary<string, List<string>>();
-
-	private void loadGameConfig()
-	{
-	    try
-	    {
-		List<string> lines = File.ReadAllLines(@"gameData\games.config").ToList();
-
-		int GameId = 1;
-
-		foreach (string line in lines)
-		{
-		    if (line.Length > 0 && line.Substring(line.Length - 2, 1) == $"{GameId}")
-		    {
-
-		    }
-		}
-	    }
-
-	    catch (Exception E)
-	    {
-		throw (ErrorHandler.GetException(E));
-	    }
-	}
-
-	private void updateGameConfig()
-	{
-	    try
-	    {
-		configValidation();
-
-		if (!doesGameConfigExist())
-		{
-		    NoGames.Visible = (true);
-
-		    loadGameConfig();
-		}
-
-		else
-		{
-		    // Show no game found, add now text in center of container.
-		}
-	    }
-
-	    catch (Exception E)
-	    {
-		throw (ErrorHandler.GetException(E));
-	    }
-	}
-
+	private readonly DashConfig GameConfig = new DashConfig();
 	private readonly Form MainContainer = new Form();
 	private readonly Label NoGames = new Label();
+
+	private class DashConfig
+	{
+	    public readonly Dictionary<int, List<string>> GameData = new Dictionary<int, List<string>>();
+
+	    public void ReadGames()
+	    {
+		try
+		{
+		    GameData.Clear();
+
+		    var RawData = File.ReadAllLines("gameData\\games.config", );
+		}
+
+		catch (Exception E)
+		{
+		    throw (ErrorHandler.GetException(E));
+		}
+	    }
+	}
 
 	private void initializeMainContainer()
 	{
@@ -161,26 +93,19 @@ namespace GamePanelX
 		throw (ErrorHandler.GetException(E));
 	    }
 
-	    var NoGamesText = string.Format("No Games have yet been added :c\r\n\r\nClick this text to add one right now!");
-	    var NoGamesLocation = new Point(-1, -1);
+	    var NoGamesText = string.Format("No Games have yet been added :c\r\nClick this text to add one right now!");
+	    var NoGamesLocation = new Point(-2, -2);
 	    var NoGamesBColor = MainContainer.BackColor;
 	    var NoGamesFColor = Color.White;
 
 	    try
 	    {
 		Control.Label(MainContainer, NoGames, Size.Empty, NoGamesLocation, NoGamesBColor, NoGamesFColor, 1, 12, NoGamesText);
-	    }
 
-	    catch (Exception E)
-	    {
-		throw (ErrorHandler.GetException(E));
-	    }
+		NoGames.Left -= 8;
+		NoGames.Top -= 8;
 
-	    try
-	    {
-		updateGameConfig();
-
-
+		NoGames.TextAlign = ContentAlignment.MiddleCenter;
 	    }
 
 	    catch (Exception E)
