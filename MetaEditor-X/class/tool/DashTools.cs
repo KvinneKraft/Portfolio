19e75958cd301e6d49ce85c86c43e91c42f014fd
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-06 20:46:06 +0200

    required libraries

---
+++ MetaEditor-X/class/tool/DashTools.cs
@@ -0,0 +1,191 @@
+﻿// Author: Dashie
+// Version: 1.0
+//
+// <description>
+//
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+using MetaEditorX.Properties;
+
+namespace MetaEditorX
+{
+    public class DashTools
+    {
+	public Size GetFontSize(string Text, int Size)
+	{
+	    return TextRenderer.MeasureText(Text, GetFont(1, Size));
+	}
+
+	public void Resize(Control Object, Size Size)
+	{
+	    Object.MaximumSize = Size;
+	    Object.MinimumSize = Size;
+	}
+
+	public void PaintRectangle(Control Object, int Thickness, Size Size, Point Location, Color Color)
+	{
+	    Object.Paint += (s, e) =>
+	    {
+		var graphics = e.Graphics;
+
+		graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+		using (Pen pen = new Pen(Color, Thickness))
+		{
+		    graphics.DrawRectangle(pen, new Rectangle(Location, Size));
+		};
+	    };
+	}
+
+	public void PaintLine(Control Object, Color Color, int Thickness, Point Location1, Point Location2)
+	{
+	    Object.Paint += (s, e) =>
+	    {
+		var graphics = e.Graphics;
+
+		graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+		using (Pen pen = new Pen(Color, Thickness))
+		{
+		    graphics.DrawLine(pen, Location1, Location2);
+		};
+	    };
+	}
+
+	public void PaintCircle(Control Object, Color Color, int Thickness, Point Location, Size Size)
+	{
+	    Object.Paint += (s, e) =>
+	    {
+		var graphics = e.Graphics;
+
+		graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+		using (Pen pen = new Pen(Color, Thickness))
+		{
+		    graphics.DrawEllipse(pen, new RectangleF(Location, Size));
+		};
+	    };
+	}
+
+	[DllImport("User32.dll")] static extern IntPtr CreateIconFromResource(byte[] presbits, uint dwResSize, bool fIcon, uint dwVer);
+	public void UseResourceCursor(Control Object, byte[] BYTES)
+	{
+	    var curse = new Cursor(CreateIconFromResource(BYTES, (uint)BYTES.Length, false, 0x00030000));
+
+	    Object.Cursor = curse;
+	    Object.Update();
+	}
+
+	public void Interactive(Control Object, Control Target)
+	{
+	    var Location = Point.Empty;
+
+	    Object.MouseMove += (s, e) =>
+	    {
+		if (Location.IsEmpty)
+		{
+		    return;
+		}
+
+		Target.Location = new Point(Target.Location.X + (e.X - Location.X), Target.Location.Y + (e.Y - Location.Y));
+	    };
+
+	    Object.MouseDown += (s, e) =>
+	    {
+		Location = new Point(e.X, e.Y);
+	    };
+
+	    Object.MouseUp += (s, e) =>
+	    {
+		Location = Point.Empty;
+	    };
+	}
+
+	public class ReadOnlyForm : Form
+	{
+	    public void PaintOwner(PaintEventArgs e)
+	    {
+		e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+		base.OnPaint(e);
+	    }
+	}
+
+	public readonly ReadOnlyForm ReadForm = new ReadOnlyForm();
+
+	public void Round(Control Object, int Radius)
+	{
+	    Object.Paint += (s, e) =>
+	    {
+		try
+		{
+		    ReadForm.PaintOwner(e);
+
+		    GraphicsPath GraphicsPath = new GraphicsPath();
+
+		    var Rectangle = new Rectangle(0, 0, Object.Width, Object.Height);
+
+		    int R = Radius * 3;
+
+		    int H = Rectangle.Height;
+		    int W = Rectangle.Width;
+
+		    int X = Rectangle.X;
+		    int Y = Rectangle.X;
+
+		    GraphicsPath.AddArc(X, Y, R, R, 170, 90);
+		    GraphicsPath.AddArc(X + W - R, Y, R, R, 270, 90);
+		    GraphicsPath.AddArc(X + W - R, Y + H - R, R, R, 0, 90);
+		    GraphicsPath.AddArc(X, Y + H - R, R, R, 80, 90);
+
+		    Object.Region = new Region(GraphicsPath);
+		}
+
+		catch (Exception E)
+		{
+		    throw (ErrorHandler.GetException(E));
+		}
+	    };
+	}
+
+	[DllImport("gdi32.dll")] private static extern IntPtr AddFontMemResourceEx(IntPtr pbFont, uint cbFont, IntPtr pdv, [In] ref uint pcFonts);
+	readonly PrivateFontCollection FontCollection = new PrivateFontCollection();
+
+	public Font GetFont(int FontId, int Height)
+	{
+	    if (FontCollection.Families.Length < 2)
+	    {
+		var RawDataCollection = new List<byte[]>()
+		{
+		    Resources.main,
+		    Resources.cute,
+		};
+
+		for (int k = 0; k < RawDataCollection.Count; k += 1)
+		{
+		    byte[] RawData = RawDataCollection[k];
+
+		    var Pointer = Marshal.AllocCoTaskMem(RawData.Length);
+
+		    Marshal.Copy(RawData, 0, Pointer, RawData.Length);
+
+		    uint Reference = 0;
+
+		    AddFontMemResourceEx(Pointer, (uint)RawData.Length, IntPtr.Zero, ref Reference);
+		    FontCollection.AddMemoryFont(Pointer, RawData.Length);
+		};
+	    };
+
+	    return new Font(FontCollection.Families[FontId], Height, FontStyle.Regular);
+	}
+    }
+}
---
+++ MetaEditor-X/class/tool/DropDownMenu.cs
@@ -0,0 +1,298 @@
+﻿// Author: Dashie
+// Version: 1.0
+//
+// <description>
+//
+
+using System;
+using System.IO;
+using System.Net;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+using MetaEditorX.Properties;
+
+namespace MetaEditorX
+{
+    public class DropDownMenu
+    {
+	readonly DashControls Control = new DashControls();
+	readonly DashTools Tool = new DashTools();
+
+	public void Hide()
+	{
+	    try
+	    {
+		Container.Hide();
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+	public void Show()
+	{
+	    try
+	    {
+		Container.Show();
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+	public readonly PictureBox ContentContainer = new PictureBox();
+	public readonly PictureBox Container = new PictureBox();
+
+	public void SetupMenu(Control Top, Point MenuLocation, Color MenuColor, Color MenuBorderColor)
+	{
+	    var MContainerSize = new Size(10, 0);
+	    var MContainerLocation = MenuLocation;
+	    var MContainerBackColor = MenuColor;
+
+	    try
+	    {
+		Control.Image(Top, Container, MContainerSize, MContainerLocation, MContainerBackColor);
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+
+	    var CContainerSize = new Size(MContainerSize.Width - 4, MContainerSize.Height - 4);
+	    var CContainerLocation = new Point(2, 2);
+	    var CContainerBackColor = Color.White;//MenuColor;
+
+	    try
+	    {
+		Control.Image(Container, ContentContainer, CContainerSize, CContainerLocation, CContainerBackColor);
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+
+	    var CRectangleSize = new Size(MContainerSize.Width - 2, MContainerSize.Height - 2);
+	    var CRectangleLocation = new Point(1, 1);
+	    var CRectangleBackColor = MenuBorderColor;
+
+	    try
+	    {
+		Tool.PaintRectangle(Container, 2, CRectangleSize, CRectangleLocation, CRectangleBackColor);
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+
+	    Top.MouseEnter += (s, e) =>
+	    {
+		if (Container.Visible)
+		{
+		    Hide();
+		}
+	    };
+
+	    Hide();
+	}
+
+	public readonly Dictionary<int, Label> MenuItems = new Dictionary<int, Label>();
+	
+	private int GetY()
+	{
+	    if (MenuItems.Count > 0)
+	    {
+		var Item = ContentContainer.Controls[ContentContainer.Controls.Count - 1];
+		return (Item.Top + Item.Height);
+	    }
+
+	    return 0;
+	}
+
+	public void AddItem(Label Object, string ItemName, Color ItemBColor, Color ItemFColor, int Index = -2, int ItemWidth = -2, int ItemHeight = -2, int ItemTextSize = 10)
+	{
+	    if (ItemWidth == -2 || ItemHeight == -2)
+	    {
+		if (ItemWidth == -2)
+		{
+		    ItemWidth = ContentContainer.Width;
+		}
+
+		if (ItemHeight == -2)
+		{
+		    ItemHeight = Tool.GetFontSize(ItemName, ItemTextSize).Height + 6;
+		}
+	    }
+
+	    var ItemSize = new Size(ItemWidth, ItemHeight);
+	    var ItemLocation = new Point(0, GetY());
+	    var ItemBackColor = ItemBColor;
+	    var ItemForeColor = ItemFColor;
+
+	    try
+	    {
+		Control.Label(ContentContainer, Object, ItemSize, ItemLocation, ItemBackColor, ItemForeColor, 1, ItemTextSize, ItemName);
+		Object.TextAlign = ContentAlignment.TopCenter;
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+
+	    int GetContentContainerWidth()
+	    {
+		if (ItemSize.Width > ContentContainer.Width)
+		{
+		    return (ItemSize.Width);
+		}
+
+		return (ContentContainer.Width);
+	    }
+
+	    var CContainerSize = new Size(GetContentContainerWidth(), ContentContainer.Height + ItemHeight);
+	    var MContainerSize = new Size(CContainerSize.Width + 4, CContainerSize.Height + 4);
+
+	    try
+	    {
+		Tool.Resize(ContentContainer, CContainerSize);
+		Tool.Resize(Container, MContainerSize);
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+
+	    if (Index == -2)
+	    {
+		Index = MenuItems.Count;
+	    }
+
+	    MenuItems.Add(Index, Object);
+	}
+
+	public Label GetItem(int Index = -2)
+	{
+	    try
+	    {
+		if (Index == -2)
+		{
+		    Index = MenuItems.Count - 1;
+		}
+
+		if (MenuItems.Count - 1 < Index)
+		{
+		    return null;
+		}
+
+		return MenuItems[Index];
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+	private void ReloadDropDownMenu()
+	{
+	    try
+	    {
+		var ItemLocation = new Point(0, 0);
+		var MenuItems = new List<Label>();
+
+		foreach (Label MenuItem in this.MenuItems.Values)
+		{
+		    MenuItem.Location = ItemLocation;
+		    
+		    ItemLocation.Y += MenuItem.Height;
+
+		    MenuItems.Add(MenuItem);
+		}
+
+		this.MenuItems.Clear();
+
+		for (int k = 0; k < MenuItems.Count; k += 1)
+		{
+		    this.MenuItems.Add(k, MenuItems[k]);
+		}
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+	public bool RemoveItem(int Index = -2)
+	{
+	    try
+	    {
+		if (Index == -2)
+		{
+		    var Count = MenuItems.Count;
+
+		    if (Count >= 1)
+		    {
+			Count -= 1;
+		    }
+
+		    Index = Count;
+		}
+
+		if (!MenuItems.ContainsKey(Index))
+		{
+		    return false;
+		}
+
+		ContentContainer.Controls.RemoveAt(Index);
+		MenuItems.Remove(Index);
+
+		ReloadDropDownMenu();
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+
+	    int GetMenuHeight()
+	    {
+		var Height = 0;
+
+		if (MenuItems.Count > 0)
+		{
+		    Height = MenuItems[MenuItems.Count - 1].Top + MenuItems[MenuItems.Count - 1].Height - 4;
+		}
+
+		return Height;
+	    }
+
+	    var CContainerSize = new Size(ContentContainer.Width, GetMenuHeight());
+	    var MContainerSize = new Size(CContainerSize.Width + 4, CContainerSize.Height + 4);
+
+	    try
+	    {
+		Tool.Resize(ContentContainer, CContainerSize);
+		Tool.Resize(Container, MContainerSize);
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+
+	    return true;
+	}
+    }
+}
