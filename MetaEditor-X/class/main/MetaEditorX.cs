db7e62380b66452b2f1455f8b5e1ec527013db19
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-06 20:45:02 +0200

    create MetaEditorX

---
+++ MetaEditor-X/class/main/MetaEditorX.cs
@@ -0,0 +1,33 @@
+﻿// Author: Dashie
+// Version: 1.0
+//
+// <description>
+//
+
+using System;
+using System.IO;
+using System.Net;
+using System.Drawing;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace MetaEditorX
+{
+    public partial class MetaEditorX
+    {
+	public void Show()
+	{
+	    try
+	    {
+
+	    }
+
+	    catch (Exception E)
+	    {
+
+	    }
+	}
+    }
+}
