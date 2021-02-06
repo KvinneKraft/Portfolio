4ec0a828fe7e012450fd1749bed2516a6e14fac0
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-06 20:51:51 +0200

    added initmetaeditorx

---
+++ MetaEditor-X/class/main/InitMetaEditorX.cs
@@ -0,0 +1,22 @@
+﻿// Author: Dashie
+// Version: 1.0
+//
+// <description>
+//
+
+using System;
+using System.IO;
+using System.Net;
+using System.Drawing;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace MetaEditorX
+{
+    public class InitMetaEditorX
+    {
+	// Design
+    }
+}
