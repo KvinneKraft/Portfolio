30d9417985887db6fc3500a6d92d02232aacafb5
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-06 20:45:34 +0200

    integrated custom launcher

---
+++ MetaEditor-X/class/main/startup/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetaEditorX
+{
+    public static class Startup
+    {
+	private static readonly MetaEditorX MetaEditorX = new MetaEditorX();
+
+	[STAThread]
+	public static void Main()
+	{
+	    Application.EnableVisualStyles();
+	    Application.SetCompatibleTextRenderingDefault(false);
+
+	    MetaEditorX.Show();
+	}
+    }
+}
