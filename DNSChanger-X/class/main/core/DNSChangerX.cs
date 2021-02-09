f9e7feb99ae464f0e232553944cf98a0d7e6d2ed
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-09 22:15:35 +0200

    moved file

---
+++ DNSChanger-X/class/main/core/DNSChangerX.cs
@@ -0,0 +1,38 @@
+﻿
+// Author: Dashie
+// Version: 1.0
+
+using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Drawing;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace DNSChangerX
+{
+    public partial class DNSChangerX
+    {
+	private readonly Initialize Initialize = new Initialize();
+
+	public void Show()
+	{
+	    try
+	    {
+		Initialize.CoreComponent();
+		Initialize.TopComponent();
+		Initialize.BottomComponent();
+
+		Application.Run(Initialize.DashDialog);
+	    }
+
+	    catch (Exception E)
+	    {
+		ErrorHandler.JustDoIt(E);
+	    }
+	}
+    }
+}
