877340875851e08c81a6f91256b70b3e5219e40e
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-09 22:32:10 +0200

    creation info class

--- DNSChanger-X/class/main/info/Info.cs
+++ DNSChanger-X/class/main/info/Info.cs
@@ -16,11 +16,40 @@ namespace DNSChangerX
 {
     public class AppInfo
     {
-	private readonly LogContainer LogContainer = new LogContainer();
+	private string AppInfo()
+	{
+	    return string.Format
+	    (
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	    );
+	}
 
 	public void Show()
 	{
+	    try
+	    {
+		var LogTitle = ("App Information");
+		var LogSize = new Size(250, 250);
 
+		using (var LogContainer = new LogContainer(LogSize, LogTitle))
+		{
+		    var ContainerBColor = Color.FromArgb(12, 12, 12);
+		    var MenuBarBColor = Color.FromArgb(8, 8, 8);
+		    var AppBColor = Color.FromArgb(20, 20, 20);
+
+		    LogContainer.Show(AppInfo(), LogTitle, MenuBarBColor, ContainerBColor, AppBColor);
+		}
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
 	}
     }
 }
