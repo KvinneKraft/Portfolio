5df79c9d39cf6ae509ae3b155ada78be6203d7e0
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-09 21:58:11 +0200

    creation initialization class

---
+++ DNSChanger-X/class/main/gui/InitDNSChangerX.cs
@@ -0,0 +1,65 @@
+﻿
+// Author: Dashie
+// Version: 1.0
+
+using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Drawing;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace DNSChangerX
+{
+    public class Initialize
+    {
+	private readonly DashControls Control = new DashControls();
+	private readonly DashTools Tool = new DashTools();
+
+	public readonly DashDialog DashDialog = new DashDialog();
+
+	public void CoreComponent()
+	{
+	    try
+	    {
+
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+	public void TopComponent()
+	{
+	    try
+	    {
+
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+	public void BottomComponent()
+	{
+	    try
+	    {
+
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+    }
+}
+
+
