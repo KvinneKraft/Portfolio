08d3c14e1888a4fe9a844656cf09480272fcf8c8
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-09 22:16:12 +0200

    creation info.cs

---
+++ DNSChanger-X/class/main/info/Info.cs
@@ -0,0 +1,26 @@
+﻿
+// Author: Dashie
+// Version: 1.0
+
+using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Drawing;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace DNSChangerX
+{
+    public class AppInfo
+    {
+	private readonly LogContainer LogContainer = new LogContainer();
+
+	public void Show()
+	{
+
+	}
+    }
+}
