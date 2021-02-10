d8f22f8c1a581572a03dd0bcc3a33ab78d39777b
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-10 22:44:49 +0200

    added left off code

---
+++ DNSChanger-X/class/main/dnslist/InitDnsList.cs
@@ -0,0 +1,53 @@
+﻿
+// Author: Dashie
+// Version: 1.0
+
+using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Drawing;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace DNSChangerX
+{
+    public class InitDnsList
+    {
+	private readonly DashControls Control = new DashControls();
+	private readonly DashTools Tool = new DashTools();
+
+
+	public readonly DashDialog DashDialog = new DashDialog();
+
+	public void CoreComponent()
+	{
+	    try
+	    {
+		//Gui Initialization
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+
+
+	public void DisplayComponent()
+	{
+	    try
+	    {
+		// DNS Servers from embedded file auto add control thing.
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+    }
+}
