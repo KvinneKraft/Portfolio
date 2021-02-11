737edc8021ba114ba6de2f4f0af41e98fb898025
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-11 22:15:45 +0200

    moved dnslist to own class

---
+++ DNSChanger-X/class/main/dnslist/DnsList.cs
@@ -0,0 +1,42 @@
+﻿
+// Author: Dashie
+// Version: 1.0
+
+using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Drawing;
+using System.Threading;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace DNSChangerX
+{
+    public class DnsList
+    {
+	public void HandleDnsList()
+	{
+	    try
+	    {
+		using (Process Process = new Process())
+		{
+		    Process.StartInfo = new ProcessStartInfo()
+		    {
+			FileName = "https://public-dns.info/nameservers.txt",
+			UseShellExecute = true,
+		    };
+
+		    Process.Start();
+		}
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+    }
+}
