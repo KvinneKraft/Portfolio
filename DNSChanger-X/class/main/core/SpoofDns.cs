621b8f67247550c286430e3843073d720a019f65
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-11 22:59:35 +0200

    Create SpoofDns.cs

---
+++ DNSChanger-X/class/main/core/SpoofDns.cs
@@ -0,0 +1,34 @@
+﻿
+// Author: Dashie
+// Version: 1.0
+
+using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Drawing;
+using System.Threading;
+using System.Management;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace DNSChangerX
+{
+    public class SpoofDns
+    {
+	public void ChangeDns(int mode, string ip1, string ip2)
+	{
+	    try
+	    {
+
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+    }
+}
