12c9c0ddb6943f7ba73b8f3db233b415aeaa5b9a
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-15 06:26:58 +0200

    adding bones

---
+++ TheDashlorisX/class/main/core/InitThaDashlorisX.cs
@@ -0,0 +1,47 @@
+﻿
+// Author: Dashie
+// Version: 3.0
+
+using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Collections;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace TheDashlorisX
+{
+    public class InitThaDashlorisX
+    {
+	public void InitMainComponent()
+	{
+
+	}
+	
+	public void SideBarComponent()
+	{
+
+	}
+
+	public void BottomBarComponent()
+	{
+
+	}
+
+	public void ContainerComponent()
+	{
+	    try
+	    {
+
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+    }
+}
