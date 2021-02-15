801608f0fefd28cb4acd19228191873028b41b3d
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-15 06:26:45 +0200

    hooked up with core

---
+++ TheDashlorisX/class/main/startup/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheDashlorisX
+{
+    static class Startup
+    {
+	[STAThread]
+	static void Main()
+	{
+	    Application.EnableVisualStyles();
+	    Application.SetCompatibleTextRenderingDefault(false);
+
+	    ThaDashloris ThaDashloris = new ThaDashloris();
+
+	    ThaDashloris.Show();
+	}
+    }
+}
