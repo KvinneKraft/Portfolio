928a307c0846174d6135a6a1ac2b56125bc6d1b7
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-19 21:33:56 +0200

    added init components

--- TheDashlorisX/class/main/core/init/pages/isonline/IsOnline.cs
+++ TheDashlorisX/class/main/core/init/pages/isonline/IsOnline.cs
@@ -16,9 +16,66 @@ namespace TheDashlorisX
 {
     public class IsOnline
     {
+	private readonly DashControls Control = new DashControls();
+	private readonly DashTools Tool = new DashTools();
+
+
+
+	private readonly PictureBox TopContainer = new PictureBox();
+
+	// topinit1
+	private readonly Label TopLabel1 = new Label();
+	private readonly Label TopLabel2 = new Label();
+	private readonly Label TopLabel3 = new Label();
+	private readonly Label TopLabel4 = new Label();
+
+	private readonly TextBox TextBox1 = new TextBox();
+	private readonly TextBox TextBox2 = new TextBox();
+	private readonly TextBox TextBox3 = new TextBox();
+	// dropdownmenu
+
+	private void InitContainer1()
+	{
+	    try
+	    {
+
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+
+
+	private readonly PictureBox BottomContainer = new PictureBox();
+
+	private void InitContainer2()
+	{
+	    try
+	    {
+
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
 	public void Initialize(DashDialog DashDialog, PictureBox Capsule)
 	{
+	    try
+	    {
+		InitContainer1();
+		InitContainer2();
+	    }
 
+	    catch (Exception E)
+	    {
+		ErrorHandler.JustDoIt(E);
+	    }
 	}
     }
 }
