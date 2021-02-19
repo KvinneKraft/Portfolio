3163b092325f0e239f2a5f98d9adfcae93b08a54
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-19 20:32:54 +0200

    added initialization components

--- TheDashlorisX/class/main/core/init/pages/about/About.cs
+++ TheDashlorisX/class/main/core/init/pages/about/About.cs
@@ -16,9 +16,55 @@ namespace TheDashlorisX
 {
     public class About
     {
+	private readonly DashControls Control = new DashControls();
+	private readonly DashTools Tool = new DashTools();
+
+
+
+	private void InitContainer1()
+	{
+	    try
+	    {
+
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+
+
+	private readonly PictureBox Container2 = new PictureBox();
+	private readonly TextBox TextBox1 = new TextBox();
+	private readonly Label Label1 = new Label();
+
+	private void InitContainer2()
+	{
+	    try
+	    {
+
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
 	public void Initialize(DashDialog DashDialog, PictureBox Capsule)
 	{
+	    try
+	    {
+		InitContainer1();
+		InitContainer2();
+	    }
 
+	    catch (Exception E)
+	    {
+		ErrorHandler.JustDoIt(E);
+	    }
 	}
     }
 }
