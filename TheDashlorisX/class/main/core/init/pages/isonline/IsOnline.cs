2a43ccaf8ab71e2e7851e9d2aabe4c1d3723b5ab
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-22 14:37:14 +0200

    added code layout for new

--- TheDashlorisX/class/main/core/init/pages/isonline/IsOnline.cs
+++ TheDashlorisX/class/main/core/init/pages/isonline/IsOnline.cs
@@ -19,11 +19,70 @@ namespace TheDashlorisX
 	private readonly DashControls Control = new DashControls();
 	private readonly DashTools Tool = new DashTools();
 
+	private readonly PictureBox Container2 = new PictureBox();
+	private readonly PictureBox Container3 = new PictureBox();
+	private readonly PictureBox PictureBox = new PictureBox();
+
+	private readonly TextBox TextBox1 = new TextBox();
+	private readonly TextBox TextBox2 = new TextBox();
+
+	private readonly Label Label1 = new Label();
+	private readonly Label Label2 = new Label();
+
+	private void InitSection1()
+	{
+	    try
+	    {
+		// Top Section
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+	private readonly PictureBox Container5 = new PictureBox();
+	private readonly PictureBox Container4 = new PictureBox();
+
+	private readonly Button Button1 = new Button();
+	private readonly Label Label3 = new Label();
+
+	private void InitSection2()
+	{
+	    try
+	    {
+		// Bottom Section
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+	private readonly PictureBox Container1 = new PictureBox();
+
+	private void InitSection3()
+	{
+	    try
+	    {
+		// Encapsulating Section
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
 	public void Initialize(DashDialog DashDialog, PictureBox Capsule)
 	{
 	    try
 	    {
-
+		InitSection1();
+		InitSection2();
+		InitSection3();
 	    }
 
 	    catch (Exception E)
