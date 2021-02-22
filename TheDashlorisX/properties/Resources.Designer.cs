bbcc029853382452ff11b12ba4c7cbd2d37414bb
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-22 15:18:56 +0200

    Update Resources.Designer.cs

--- TheDashlorisX/properties/Resources.Designer.cs
+++ TheDashlorisX/properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace TheDashlorisX.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.ToolStronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -77,6 +77,16 @@ namespace TheDashlorisX.Properties {
             get {
                 object obj = ResourceManager.GetObject("ICON", resourceCulture);
                 return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap isonline1 {
+            get {
+                object obj = ResourceManager.GetObject("isonline1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
         
