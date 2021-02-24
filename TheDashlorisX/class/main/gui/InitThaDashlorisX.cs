c6db71447f8f4ac23440add9b8fcc5ff1b9432bc
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-24 05:04:15 +0200

    setup main dialog

--- TheDashlorisX/class/main/gui/InitThaDashlorisX.cs
+++ TheDashlorisX/class/main/gui/InitThaDashlorisX.cs
@@ -14,11 +14,38 @@ using System.Collections.Generic;
 
 namespace TheDashlorisX
 {
-    public class InitThaDashlorisX
+    public class InitThaDashlorisX : DashGlobe
     {
 	public readonly DashDialog DashDialog = new DashDialog();
 
 	private void Init1()
+	{
+	    try
+	    {
+		var DialogMCol = Color.FromArgb(17, 38, 94);
+		var DialogBCol = Color.FromArgb(6, 14, 36);
+		var DialogSize = new Size(400, 350);
+
+		DashDialog.JustInitialize(DialogSize, ("Tha Dashloris-X  -  3.0"), DialogBCol, DialogMCol);
+
+		DashDialog.MenuBar.Close.Click += (s, e) =>
+		{
+		    ExitApplication();
+		};
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+
+	private readonly PictureBox S2Container1 = new PictureBox();//Sector 2 Container 1
+	private readonly PictureBox S2Container2 = new PictureBox();
+	private readonly PictureBox S2Container3 = new PictureBox();
+
+	private void Init2()
 	{
 	    try
 	    {
@@ -37,11 +64,14 @@ namespace TheDashlorisX
 	    try
 	    {
 		Init1();//GUI
+		Init2();//BottomBar
+		//Init3();//SideMenu
+		//Init4();//Capsule
 	    }
 
 	    catch (Exception E)
 	    {
-		throw (ErrorHandler.GetException(E));
+		ErrorHandler.JustDoIt(E);
 	    }
 	}
     }
