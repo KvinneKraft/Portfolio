7b91a315ce16c6fa0ecfbb6f7b1a2dcf60693be0
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-24 04:47:28 +0200

    Update AssemblyInfo.cs

--- TheDashlorisX/properties/AssemblyInfo.cs
+++ TheDashlorisX/properties/AssemblyInfo.cs
@@ -1,36 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("TheDashlorisX")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TheDashloris-X")]
+[assembly: AssemblyDescription("Stress Tester")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TheDashlorisX")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("KvinneKraft")]
+[assembly: AssemblyProduct("TheDashloris-X 3.0")]
+[assembly: AssemblyCopyright("Copyright © KvinneKraft 2021")]
+[assembly: AssemblyTrademark("KvinneKraft")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ca6b1b82-141f-41e0-b502-50f8a6f925c5")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
