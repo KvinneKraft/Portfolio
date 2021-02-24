77dc43d150843dbb2fc1881d63d44fcc617ceeeb
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-24 04:47:31 +0200

    Update Resources.Designer.cs

--- TheDashlorisX/properties/Resources.Designer.cs
+++ TheDashlorisX/properties/Resources.Designer.cs
@@ -83,16 +83,6 @@ namespace TheDashlorisX.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap isonline1 {
-            get {
-                object obj = ResourceManager.GetObject("isonline1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap LOGO {
             get {
                 object obj = ResourceManager.GetObject("LOGO", resourceCulture);
