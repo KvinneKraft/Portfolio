f08e0a13b1e76ea23c988d7ce03660bab347932b
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-24 05:30:58 +0200

    Update Resources.Designer.cs

--- TheDashlorisX/properties/Resources.Designer.cs
+++ TheDashlorisX/properties/Resources.Designer.cs
@@ -99,5 +99,15 @@ namespace TheDashlorisX.Properties {
                 return ((byte[])(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap MenuBarIcon {
+            get {
+                object obj = ResourceManager.GetObject("MenuBarIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
