911734e41e70d4857b32e1980499502d25746762
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-24 04:46:39 +0200

    Create Startup.cs

---
+++ TheDashlorisX/class/startup/Startup.cs
@@ -0,0 +1,30 @@
+﻿
+// Author: Dashie
+// Version: 3.0
+
+using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Collections;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace TheDashlorisX
+{
+    static class Startup
+    {
+	[STAThread]
+	static void Main()
+	{
+	    Application.EnableVisualStyles();
+	    Application.SetCompatibleTextRenderingDefault(false);
+
+	    ThaDashloris ThaDashloris = new ThaDashloris();
+
+	    ThaDashloris.RunApplication();
+	}
+    }
+}
