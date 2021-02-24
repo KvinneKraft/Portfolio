def2baeb2c3cc266f902ec6ee855c5d38a64cc88
KvinneKraft <KvinneKraft@protonmail.com>
2021-02-24 04:46:36 +0200

    Create ThaDashlorisX.cs

---
+++ TheDashlorisX/class/main/ThaDashlorisX.cs
@@ -0,0 +1,33 @@
+﻿
+// Author: Dashie
+// Version: 3.0
+
+using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Collections;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace TheDashlorisX
+{
+    public partial class ThaDashloris : InitThaDashlorisX
+    {
+	public void RunApplication()
+	{
+	    try
+	    {
+
+		//Application.Run();
+	    }
+
+	    catch (Exception E)
+	    {
+		ErrorHandler.JustDoIt(E);
+	    }
+	}
+    }
+}
