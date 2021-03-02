36ddccea685e884be12f4d88d0f38a2301da988e
KvinneKraft <KvinneKraft@protonmail.com>
2021-03-02 14:04:01 +0200

    Create AppToS.cs

---
+++ TheDashlorisX/class/main/page/tos/AppToS.cs
@@ -0,0 +1,24 @@
+﻿
+// Author: Dashie
+// Version: 3.0
+
+using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Collections;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace TheDashlorisX
+{
+    public class AppToS
+    {
+	public void InitializePage(DashDialog DashDialog, PictureBox Capsule)
+	{
+
+	}
+    }
+}
