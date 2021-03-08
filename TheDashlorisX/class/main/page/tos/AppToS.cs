49d7ed3403bc2114db14382b9d45ae6a6662cc76
KvinneKraft <KvinneKraft@protonmail.com>
2021-03-08 17:29:27 +0200

    added functional pages

--- TheDashlorisX/class/main/page/tos/AppToS.cs
+++ TheDashlorisX/class/main/page/tos/AppToS.cs
@@ -28,9 +28,8 @@ namespace TheDashlorisX
 	    {
 		var ContainerSize = Capsule.Size;
 		var ContainerLoca = new Point(0, 0);
-		var ContainerBCol = Color.FromArgb(8, 8, 8);
 
-		Control.Image(Capsule, S1Container1, ContainerSize, ContainerLoca, ContainerBCol);
+		Control.Image(Capsule, S1Container1, ContainerSize, ContainerLoca, Color.MidnightBlue);
 		Tool.Round(S1Container1, 6);
 	    }
 
@@ -46,6 +45,8 @@ namespace TheDashlorisX
 	private readonly Label S2Label1 = new Label();//Title MEssage
 	private readonly Label S2Label2 = new Label();//not Necessary (1/4)
 
+	private int S2PageID = 1;
+
 	private void Init2()
 	{
 	    try
@@ -56,13 +57,13 @@ namespace TheDashlorisX
 		Control.Image(S1Container1, S2Container1, ContainerSize, ContainerLoca, Color.MidnightBlue);
 
 		var Label1Size = Tool.GetFontSize("Page:", 9);
-		var Label2Size = Tool.GetFontSize("1/10 ", 9);
+		var Label2Size = Tool.GetFontSize("1/3 ", 9);
 
 		var Label1Loca = new Point((S2Container1.Width - (Label1Size.Width + Label2Size.Width)) / 2, (S2Container1.Height - Label1Size.Height) / 2);
 		var Label2Loca = new Point(Label1Loca.X + Label1Size.Width, Label1Loca.Y);
 
 		Control.Label(S2Container1, S2Label1, Label1Size, Label1Loca, S2Container1.BackColor, Color.White, 1, 9, "Page:");
-		Control.Label(S2Container1, S2Label2, Label2Size, Label2Loca, S2Container1.BackColor, Color.White, 1, 9, "1/10");
+		Control.Label(S2Container1, S2Label2, Label2Size, Label2Loca, S2Container1.BackColor, Color.White, 1, 9, "1/3");
 	    }
 
 	    catch (Exception E)
@@ -77,6 +78,41 @@ namespace TheDashlorisX
 
 	private readonly Button S3Button1 = new Button();//Left
 	private readonly Button S3Button2 = new Button();//Right
+
+	private void S3ChangePage(bool Forward)
+	{
+	    try
+	    {
+		if (Forward)
+		{
+		    if (S2PageID > 2)
+		    {
+			return;
+		    }
+		    
+		    S4Label1.Top -= S4Container1.Height;
+		    S2PageID += 1;
+		}
+
+		else
+		{
+		    if (S2PageID < 2)
+		    {
+			return;
+		    }
+
+		    S4Label1.Top += S4Container1.Height;
+		    S2PageID -= 1;
+		}
+
+		S2Label2.Text = $"{S2PageID}" + S2Label2.Text.Substring(1, 2);
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
 
 	private readonly Label S3Label1 = new Label();
 
@@ -109,6 +145,9 @@ namespace TheDashlorisX
 		Control.Button(S3Container2, S3Button1, ButtonSize, Button1Loca, ContainerBCol, Color.White, 1, 9, ("<"));
 		Control.Button(S3Container2, S3Button2, ButtonSize, Button2Loca, ContainerBCol, Color.White, 1, 9, (">"));
 
+		S3Button1.Click += (s, e) => S3ChangePage(false);
+		S3Button2.Click += (s, e) => S3ChangePage(true);
+
 		Tool.Round(S3Button1, 6);
 		Tool.Round(S3Button2, 6);
 	    }
@@ -121,7 +160,6 @@ namespace TheDashlorisX
 
 
 	private readonly PictureBox S4Container1 = new PictureBox();
-	private readonly PictureBox S4Container2 = new PictureBox();
 
 	private readonly Label S4Label1 = new Label();
 
@@ -129,10 +167,12 @@ namespace TheDashlorisX
 	{
 	    return string.Format
 	    (
-		"1\r\n" +
-		"2\r\n" +
-		"3\r\n" +
-		"4\r\n"
+		"----( Start of Terms of Services )----\r\n\r\n" +
+		"1) Whenever you make use of this application you are automatically agreeing with any of the bellow.  Which includes me not being responsible for whatever you inflict (whether it be negative or positive) upon another using this application.  I merely claim responsibility as the owner of this software as the developer of this software.\r\n\r\n" +
+		"2) You know what you are using and why you are using it.  You also know why and how you can and cannot use this application under the law of your country.  This means you fully claim responsibility (as stated before) and hereby confirm you are aware of any missuse of this product, if any.  Missuse being any use which is not tolerated by your local government.  Local being your country, state and perhaps even city.  Depending on where you live of course.  Know that any request sent can be read and interpreted by the receiver without having to decipher any form of encryption.\r\n\r\n" +
+		"3) You know what the purpose of this project is and you also confirm that you have downloaded this piece of software from my GitHub repository.  You can find my GitHub repository at; https://github.com/KvinneKraft -Please validate any files if necessary.\r\n\r\n" +
+		"4) It is not allowed to modify any part(s) of this software, any change made to the code of this software or the assembly itself is strongly prohibited, unless done by the application itself, which if the case should be able to be traced back to my GitHub source code and therefore declare the modification as allowed.  If you do wish to modify any part of this application or simply have suggestions, then you can always reach out to me at KvinneKraft@protonmail.com.  If instead you wish to scroll through my code and see what could use some improvement then you can go to the repository linked above.\r\n\r\n" +
+		"----( End of Terms of Services )----"
 	    );
 	}
 
@@ -140,7 +180,18 @@ namespace TheDashlorisX
 	{
 	    try
 	    {
+		var ContainerSize = new Size(S1Container1.Width - 5, S1Container1.Height - S3Container1.Height - S2Container1.Height - 4);
+		var ContainerLoca = new Point(3, S2Container1.Height + 2);
+		var ContainerBCol = Color.FromArgb(24, 24, 24);
 
+		Control.Image(S1Container1, S4Container1, ContainerSize, ContainerLoca, ContainerBCol);
+
+		var LabelText = TermsOfServices();
+		var LabelFSiz = TextRenderer.MeasureText(LabelText, Tool.GetFont(1, 9), Size.Empty, flags:TextFormatFlags.WordBreak);//Tool.GetFontSize(TermsOfServices(), 9);
+		var LabelSize = new Size(ContainerSize.Width - 4, LabelFSiz.Height - 4);
+		var LabelLoca = new Point(2, 2);
+
+		Control.Label(S4Container1, S4Label1, LabelSize, LabelLoca, ContainerBCol, Color.White, 1, 9, LabelText);
 	    }
 
 	    catch (Exception E)
