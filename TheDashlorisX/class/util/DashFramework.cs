cc4d2568b020ba91e0c0d65dae495d1e83864870
KvinneKraft <KvinneKraft@protonmail.com>
2021-03-09 19:50:15 +0200

    added DashDialog, removed DashBox

--- TheDashlorisX/class/util/DashFramework.cs
+++ TheDashlorisX/class/util/DashFramework.cs
@@ -92,23 +92,122 @@ namespace DashFramework
 
     namespace Dialog
     {
-	public class DashBox
+	public class DashWindow : Form
 	{
-	    public enum Button { OKCancel, YesNo, OK }
+	    private readonly DashControls Control = new DashControls();
+	    private readonly DashTools Tool = new DashTools();
 
-	    public int ShowMe(string message, string title, Color AppBCol, Color MenuBarBCol, Color TextContainerBCol, Color TextContainerForeColor)
+	    private void InitS1(Size AppSize, string AppTitle, Color AppBCol, FormStartPosition AppStartPosition = FormStartPosition.CenterScreen, FormBorderStyle AppBorderStyle = FormBorderStyle.None)
 	    {
 		try
 		{
+		    SuspendLayout();
 
+		    Icon = Properties.Resources.ICON;
+
+		    FormBorderStyle = AppBorderStyle;
+		    StartPosition = AppStartPosition;
+
+		    MaximumSize = AppSize;
+		    MinimumSize = AppSize;
+
+		    BackColor = AppBCol;
+
+		    Text = AppTitle;
+		    Name = AppTitle;
+
+		    Tool.Round(this, 6);
 		}
 
 		catch (Exception E)
 		{
 		    throw (ErrorHandler.GetException(E));
 		}
+	    }
 
-		return -1;
+	    private readonly DashMenuBar MenuBar = null;
+
+	    private void InitS2(string AppTitle, bool AppMinim, bool AppClose, bool AppHide, Color MenuBarBCol)
+	    {
+		try
+		{
+		    MenuBar = new DashMenuBar(AppTitle, AppMinim, AppClose, AppHide);
+		    MenuBar.Add(this, 26, MenuBarBCol, MenuBarBCol);
+		}
+
+		catch (Exception E)
+		{
+		    throw (ErrorHandler.GetException(E));
+		}
+	    }
+
+	    private void DoInitializeApp(Size AppSize, string AppTitle, Color AppBCol, Color MenuBarBCol, FormStartPosition StartPosition = FormStartPosition.CenterScreen, FormBorderStyle FormBorderStyle = FormBorderStyle.None, bool MenuBarMinim = false, bool MenuBarClose = true, bool CloseHideApp = true)
+	    {
+		try
+		{
+		    InitS1(AppSize, AppTitle, AppBCol, AppStartPosition: StartPosition, AppBorderStyle: FormBorderStyle);
+		    InitS2(AppTitle, MenuBarMinim, MenuBarClose, CloseHideApp, MenuBarBCol);
+		}
+
+		catch (Exception E)
+		{
+		    throw (ErrorHandler.GetException(E));
+		}
+	    }
+
+	    private bool DoInitialize = true;
+
+	    public void Show(Size AppSize, string AppTitle, Color AppBCol, Color MenuBarBCol, FormStartPosition StartPosition = FormStartPosition.CenterScreen, FormBorderStyle FormBorderStyle = FormBorderStyle.None, bool ShowDialog = true, bool MenuBarMinim = false, bool MenuBarClose = true, bool CloseHideApp = true)
+	    {
+		try
+		{
+		    if (DoInitialize)
+		    {
+			DoInitializeApp(AppSize, AppTitle, AppBCol, MenuBarBCol, StartPosition, FormBorderStyle, MenuBarMinim, MenuBarClose, CloseHideApp);
+			DoInitialize = false;
+		    }
+
+		    ShowAsIs(ShowDialog);
+		}
+
+		catch (Exception E)
+		{
+		    throw (ErrorHandler.GetException(E));
+		}
+	    }
+
+	    public void JustInitialize(Size AppSize, string AppTitle, Color AppBCol, Color MenuBarBCol, FormStartPosition StartPosition = FormStartPosition.CenterScreen, FormBorderStyle FormBorderStyle = FormBorderStyle.None, bool MenuBarMinim = false, bool MenuBarClose = true, bool CloseHideApp = true)
+	    {
+		try
+		{
+		    DoInitializeApp(AppSize, AppTitle, AppBCol, MenuBarBCol, StartPosition, FormBorderStyle, MenuBarMinim: MenuBarMinim, MenuBarClose: MenuBarClose, CloseHideApp: CloseHideApp);
+		}
+
+		catch (Exception E)
+		{
+		    throw (ErrorHandler.GetException(E));
+		}
+	    }
+
+	    public void ShowAsIs(bool ShowDialog = true)
+	    {
+		try
+		{
+		    if (ShowDialog)
+		    {
+			this.ShowDialog();
+		    }
+
+		    else
+		    {
+			Show();
+		    }
+		}
+
+		catch (Exception E)
+		{
+		    throw (ErrorHandler.GetException(E));
+		}
 	    }
 	}
     }
