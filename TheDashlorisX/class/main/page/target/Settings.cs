21c11869168ce87320dad6914274d0c5acada9af
KvinneKraft <KvinneKraft@protonmail.com>
2021-03-16 12:54:44 +0200

    added settings method layout

---
+++ TheDashlorisX/class/main/page/target/Settings.cs
@@ -0,0 +1,111 @@
+﻿
+// Author: Dashie
+// Version: 3.0
+
+using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Collections;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+using DashFramework.Interface.Controls;
+using DashFramework.Interface.Tools;
+using DashFramework.Erroring;
+
+namespace TheDashlorisX
+{
+    public class Settings
+    {
+	private readonly DashControls Control = new DashControls();
+	private readonly DashTools Tool = new DashTools();
+
+
+	private readonly PictureBox S1Container1 = new PictureBox();//Main Capsule Container
+	private readonly PictureBox S1Container2 = new PictureBox();//Main Configuration Container
+	private readonly PictureBox S1Container3 = new PictureBox();//Main Proxy Container
+
+	private void InitS1()
+	{
+	    try
+	    {
+
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+
+	private readonly PictureBox S2Container1 = new PictureBox();//Configuration Container;
+
+	private void InitS2()
+	{
+	    try
+	    {
+
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+
+	private readonly PictureBox S3Container1 = new PictureBox();//Proxy List Container
+	private readonly PictureBox S3Container2 = new PictureBox();//Proxy Option Container
+
+	private void InitS3()
+	{
+
+	}
+
+
+	public void Initialize()
+	{
+	    try
+	    {
+		InitS1();
+		InitS2();
+		InitS3();
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+	public void Show()
+	{
+	    try
+	    {
+		S1Container1.Show();
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+	public void Hide()
+	{
+	    try
+	    {
+		S1Container1.Hide();
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+    }
+}
