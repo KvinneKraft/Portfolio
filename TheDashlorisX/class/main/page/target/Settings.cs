942f52231714e1a94ce694e17a44e533fd2fc1ff
KvinneKraft <KvinneKraft@protonmail.com>
2021-03-16 13:21:50 +0200

    added Control layout

--- TheDashlorisX/class/main/page/target/Settings.cs
+++ TheDashlorisX/class/main/page/target/Settings.cs
@@ -38,10 +38,10 @@ namespace TheDashlorisX
 
 		Control.Image(Capsule, S1Container1, Capsule.Size, Container1Loca, Container1BCol);
 
-		var ContainerSize = new Size(Capsule.Width, 105);//102 original calculation
+		var ContainerSize = new Size(Capsule.Width, 125);//102 original calculation + 20 for y:10
 		var ContainerBCol = DashWindow.MenuBar.MenuBar.BackColor;
 
-		var Container3Loca = new Point(0, ContainerSize.Height + 10);
+		var Container3Loca = new Point(0, 145);
 		var Container2Loca = new Point(0, 0);
 
 		Control.Image(S1Container1, S1Container3, ContainerSize, Container3Loca, ContainerBCol);
@@ -56,6 +56,20 @@ namespace TheDashlorisX
 
 
 	private readonly PictureBox S2Container1 = new PictureBox();//Configuration Container;
+	private readonly PictureBox S2CheckBox1 = new PictureBox();
+
+	private readonly TextBox S2Textbox1 = new TextBox();
+	private readonly TextBox S2Textbox2 = new TextBox();
+	private readonly TextBox S2Textbox3 = new TextBox();
+	private readonly TextBox S2Textbox4 = new TextBox();
+	private readonly TextBox S2Textbox5 = new TextBox();
+
+	private readonly Label S2Label1 = new Label();
+	private readonly Label S2Label2 = new Label();
+	private readonly Label S2Label3 = new Label();
+	private readonly Label S2Label4 = new Label();
+	private readonly Label S2Label5 = new Label();
+	private readonly Label S2Label6 = new Label();
 
 	private void InitS2()
 	{
@@ -74,9 +88,26 @@ namespace TheDashlorisX
 	private readonly PictureBox S3Container1 = new PictureBox();//Proxy List Container
 	private readonly PictureBox S3Container2 = new PictureBox();//Proxy Option Container
 
+	private readonly TextBox S3TextBox1 = new TextBox();
+	private readonly TextBox S3TextBox2 = new TextBox();
+
+	private readonly Button S3Selector1 = new Button();
+	private readonly Button S3Selector2 = new Button();
+
+	private readonly Label S3Label1 = new Label();
+	private readonly Label S3Label2 = new Label();
+
 	private void InitS3()
 	{
+	    try
+	    {
 
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
 	}
 
 
