ad0671b45391b2513b26c9dac533a26cbba08123
KvinneKraft <KvinneKraft@protonmail.com>
2021-03-22 23:49:16 +0200

    added two main containers

--- TheDashlorisX/class/main/page/ports/PortScan.cs
+++ TheDashlorisX/class/main/page/ports/PortScan.cs
@@ -30,11 +30,28 @@ namespace TheDashlorisX
 	private readonly PictureBox S1Container2 = new PictureBox();
 	private readonly PictureBox S1Container3 = new PictureBox();
 
-	private void Init1()
+	private void Init1(DashWindow DashWindow, PictureBox Capsule)
 	{
 	    try
 	    {
+		var Container1Size = Capsule.Size;
+		var Container1Loca = new Point(0, 0);
 
+		Control.Image(Capsule, S1Container1, Container1Size, Container1Loca, Capsule.BackColor);
+
+		var Container2Size = new Size(Capsule.Width, 110);
+		var Container2Loca = new Point(0, 0);
+
+		var Container3Size = new Size(Capsule.Width, 125);
+		var Container3Loca = new Point(0, 120);
+
+		var ContainerBCol = DashWindow.MenuBar.MenuBar.BackColor;
+
+		Control.Image(S1Container1, S1Container2, Container2Size, Container2Loca, ContainerBCol);
+		Control.Image(S1Container1, S1Container3, Container3Size, Container3Loca, ContainerBCol);
+
+		Tool.Round(S1Container2, 6);
+		Tool.Round(S1Container3, 6);
 	    }
 
 	    catch (Exception E)
@@ -43,6 +60,20 @@ namespace TheDashlorisX
 	    }
 	}
 
+
+	private readonly PictureBox S2Container1 = new PictureBox();
+	private readonly PictureBox S2Container2 = new PictureBox();
+
+	private readonly TextBox S2TextBox1 = new TextBox();
+	private readonly TextBox S2TextBox2 = new TextBox();
+	private readonly TextBox S2TextBox3 = new TextBox();
+
+	private readonly Label S2Label1 = new Label();
+	private readonly Label S2Label2 = new Label();
+	private readonly Label S2Label3 = new Label();
+	private readonly Label S2Label4 = new Label();
+	private readonly Label S2Label5 = new Label();
+	private readonly Label S2Label6 = new Label();
 
 	private void Init2()
 	{
@@ -57,6 +88,15 @@ namespace TheDashlorisX
 	    }
 	}
 
+
+	private readonly PictureBox S3Container1 = new PictureBox();
+	private readonly PictureBox S3Container2 = new PictureBox();
+
+	private readonly TextBox S3TextBox1 = new TextBox();
+
+	private readonly Button S3Button1 = new Button();
+
+	private readonly Label S3Label1 = new Label();
 
 	private void Init3()
 	{
@@ -80,6 +120,10 @@ namespace TheDashlorisX
 	    {
 		if (!isInitialized)
 		{
+
+		    Init1(DashWindow, Capsule);
+		    Init2();
+		    Init3();
 
 		    isInitialized = true;
 		}
