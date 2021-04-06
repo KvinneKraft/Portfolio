b3b9959a3b319079db4a8ff040df3b5e276a3675
KvinneKraft <KvinneKraft@protonmail.com>
2021-04-06 13:31:53 +0300

    created attacclog class for log

---
+++ TheDashlorisX/class/main/page/target/AttaccLog.cs
@@ -0,0 +1,33 @@
+﻿
+// Author: Dashie
+// Version: 3.0
+
+using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Collections;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+using DashFramework.Interface.Controls;
+using DashFramework.Interface.Tools;
+
+using DashFramework.Erroring;
+using DashFramework.Dialog;
+
+namespace TheDashlorisX
+{
+    public class AttaccLog
+    {
+	private readonly DashControls Control = new DashControls();
+	private readonly DashTools Tool = new DashTools();
+
+	public void CommenceLaunch()
+	{
+
+	}
+    }
+}
