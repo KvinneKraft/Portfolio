def744488b5ff6311dd85dbf5bd5e7551f80c3f3
KvinneKraft <KvinneKraft@protonmail.com>
2021-04-06 15:23:30 +0300

    added back-bone of attaclog

--- TheDashlorisX/class/main/page/target/logger/AttaccLog.cs
+++ TheDashlorisX/class/main/page/target/logger/AttaccLog.cs
@@ -8,6 +8,7 @@ using System.Net;
 using System.Linq;
 using System.Text;
 using System.Drawing;
+using System.Threading;
 using System.Collections;
 using System.Windows.Forms;
 using System.Collections.Generic;
@@ -22,20 +23,61 @@ namespace TheDashlorisX
 {
     public class AttaccLog
     {
-	private readonly DashControls Control = new DashControls();
-	private readonly DashTools Tool = new DashTools();
-
-	public void CommenceLaunch()
-	{
-
-	}
-	
-	// Put in different file:
 	public class LogGUI
 	{
+	    private readonly DashControls Control = new DashControls();
+	    private readonly DashTools Tool = new DashTools();
+
+
 	    public void Show()
 	    {
 
+	    }
+	}
+
+	public AttaccLog()
+	{
+	    try
+	    {
+
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+
+	public bool KeepStressing = true;
+
+	public void CommenceLaunch(LockOn S3Class1, Settings S3Class2, DashWindow DashWindow, PictureBox Capsule)
+	    //string Host, string Port, string Duration, string HTTPv, string SendDelay, string Timeout, 
+	    //string DashWorkers, string MaxConnections, string ContentLength, bool UAR, List<string> ProxyList, 
+	    //List<string> CredentialList)
+	{
+	    try
+	    {
+
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+	public void StopAttack()
+	{
+	    try
+	    {
+		KeepStressing = false;
+		Thread.Sleep(1250);
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
 	    }
 	}
     }
