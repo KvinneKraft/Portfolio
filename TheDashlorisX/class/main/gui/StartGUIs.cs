0c0ddd1962c97cae093b3f861969860ff1445aa3
KvinneKraft <KvinneKraft@protonmail.com>
2021-04-20 03:41:54 +0300

    added already running dialog

--- TheDashlorisX/class/main/gui/StartGUIs.cs
+++ TheDashlorisX/class/main/gui/StartGUIs.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Drawing;
 using System.Threading;
 using System.Collections;
+using System.Diagnostics;
 using System.Windows.Forms;
 using System.Collections.Generic;
 
@@ -17,10 +18,11 @@ using DashFramework.Interface.Controls;
 using DashFramework.Interface.Tools;
 
 using DashFramework.Erroring;
+using DashFramework.System;
 using DashFramework.Dialog;
 
 namespace TheDashlorisX
-{
+{//put into single class:
     public class InsufficientPermissions
     {
 	private readonly DashControls Control = new DashControls();
@@ -47,7 +49,7 @@ namespace TheDashlorisX
 		Label1.TextAlign = ContentAlignment.MiddleCenter;
 
 		var ContainerSize = new Size(180, 20);
-		var ContainerLoca = new Point((DashWindow.Width - 200) / 2, LabelSize.Height + LabelLoca.Y + 15);
+		var ContainerLoca = new Point((DashWindow.Width - 180) / 2, LabelSize.Height + LabelLoca.Y + 15);
 		var ContainerBCol = DashWindow.BackColor;
 
 		Control.Image(DashWindow, Container, ContainerSize, ContainerLoca, ContainerBCol);
@@ -61,6 +63,30 @@ namespace TheDashlorisX
 
 		Control.Button(Container, Button1, ButtonSize, Button1Loca, ButtonBCol, ButtonFCol, 1, 8, ("Yes"));
 		Control.Button(Container, Button2, ButtonSize, Button2Loca, ButtonBCol, ButtonFCol, 1, 8, ("No"));
+
+		Button1.Click += (s, e) =>
+		{
+		    using (Process Proc = new Process())
+		    {
+			DashInteract Interact = new DashInteract();
+
+			Proc.StartInfo = new ProcessStartInfo()
+			{
+			    FileName = Interact.GetFilePath(),
+			    UseShellExecute = true,
+			    Verb = "runas"
+			};
+
+			Proc.Start();
+
+			Application.Exit();
+		    }
+		};
+
+		Button2.Click += (s, e) =>
+		{
+		    Application.Exit();
+		};
 
 		Tool.Round(Button1, 6);
 		Tool.Round(Button2, 6);
@@ -78,7 +104,7 @@ namespace TheDashlorisX
 	    {
 		using (DashWindow DashWindow = new DashWindow())
 		{
-		    var DashWindowSize = new Size();
+		    var DashWindowSize = new Size(300, 185);
 		    var DashWindowBCol = Color.FromArgb(6, 14, 36);
 		    var DashWindowMCol = Color.FromArgb(17, 38, 94);
 
@@ -102,6 +128,79 @@ namespace TheDashlorisX
 
     public class AlreadyRunning
     {
+	private readonly DashControls Control = new DashControls();
+	private readonly DashTools Tool = new DashTools();
 
+	private readonly PictureBox Container = new PictureBox();
+
+	private readonly Button Button1 = new Button();
+	
+	private readonly Label Label1 = new Label();
+
+	private void LoadSubComponents(DashWindow DashWindow)
+	{
+	    try
+	    {
+		string message = "There is already an instance of me in the background.\r\n\r\nClose him first if you want to pass me!";
+
+		var LabelSize = new Size(DashWindow.Width - 20, 95);
+		var LabelLoca = new Point(10, DashWindow.MenuBar.MenuBar.Height + 10);
+
+		Control.Label(DashWindow, Label1, LabelSize, LabelLoca, DashWindow.BackColor, Color.White, 1, 10, (message));
+
+		Label1.TextAlign = ContentAlignment.MiddleCenter;
+
+		var ContainerSize = new Size(85, 20);
+		var ContainerLoca = new Point((DashWindow.Width - 85) / 2, LabelSize.Height + LabelLoca.Y + 15);
+		var ContainerBCol = DashWindow.BackColor;
+
+		Control.Image(DashWindow, Container, ContainerSize, ContainerLoca, ContainerBCol);
+
+		var ButtonSize = new Size(85, 20);
+		var ButtonLoca = new Point(0, 0);
+		var ButtonFCol = Color.White;
+		var ButtonBCol = DashWindow.MenuBar.MenuBar.BackColor;
+
+		Control.Button(Container, Button1, ButtonSize, ButtonLoca, ButtonBCol, ButtonFCol, 1, 8, ("Okay"));
+
+		Button1.Click += (s, e) =>
+		{
+		    Application.Exit();
+		};
+
+		Tool.Round(Button1, 6);
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+	public AlreadyRunning()
+	{
+	    try
+	    {
+		using (DashWindow DashWindow = new DashWindow())
+		{
+		    var DashWindowSize = new Size(300, 187);
+		    var DashWindowBCol = Color.FromArgb(6, 14, 36);
+		    var DashWindowMCol = Color.FromArgb(17, 38, 94);
+
+		    DashWindow.InitializeWindow(DashWindowSize, ("Process is already running!"), DashWindowBCol, DashWindowMCol, CloseHideApp: false);
+
+		    LoadSubComponents(DashWindow);
+
+		    DashWindow.ShowAsIs();
+		}
+
+		Application.Exit();
+	    }
+
+	    catch (Exception E)
+	    {
+		ErrorHandler.JustDoIt(E);
+	    }
+	}
     }
 }
