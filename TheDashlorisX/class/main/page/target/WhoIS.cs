1dab7628116e55c1a664122069eccb47d00cd7c2
KvinneKraft <KvinneKraft@protonmail.com>
2021-04-26 23:44:41 +0300

    added capsule container

--- TheDashlorisX/class/main/page/target/WhoIS.cs
+++ TheDashlorisX/class/main/page/target/WhoIS.cs
@@ -28,7 +28,53 @@ namespace TheDashlorisX
 
 	private readonly PictureBox S1Container1 = new PictureBox();
 
-	private void Init1()
+	private void Init1(PictureBox Capsule, DashWindow DashWindow)
+	{
+	    try
+	    {
+		var ContainerSize = new Size(Capsule.Width, Capsule.Height);
+		var ContainerLoca = new Point(0, 0);
+
+		Control.Image(Capsule, S1Container1, ContainerSize, ContainerLoca, Capsule.BackColor);
+		
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+	
+
+	private readonly PictureBox S2Container1 = new PictureBox();
+	private readonly PictureBox S2Container2 = new PictureBox();
+
+	private readonly TextBox S2TextBox1 = new TextBox();
+
+	private readonly Label S2Label1 = new Label();
+
+	private void Init2()
+	{
+	    try
+	    {
+
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+
+	private readonly PictureBox S3Container1 = new PictureBox();
+	private readonly PictureBox S3Container2 = new PictureBox();
+
+	private readonly TextBox S3TextBox1 = new TextBox();
+
+	private readonly Label S3Label1 = new Label();
+
+	private void Init3()
 	{
 	    try
 	    {
@@ -50,7 +96,9 @@ namespace TheDashlorisX
 	    {
 		if (needInit)
 		{
-
+		    Init1(Capsule, DashWindow);
+		    Init2();
+		    Init3();
 
 		    needInit = false;
 		}
