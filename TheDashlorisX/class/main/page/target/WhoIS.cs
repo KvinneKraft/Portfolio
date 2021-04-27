9f065fd5cd168e62e846678450f99f52db205416
KvinneKraft <KvinneKraft@protonmail.com>
2021-04-27 22:52:11 +0300

    added 2 main containers

--- TheDashlorisX/class/main/page/target/WhoIS.cs
+++ TheDashlorisX/class/main/page/target/WhoIS.cs
@@ -8,6 +8,7 @@ using System.Net;
 using System.Linq;
 using System.Text;
 using System.Drawing;
+using System.Diagnostics;
 using System.Collections;
 using System.Windows.Forms;
 using System.Collections.Generic;
@@ -49,15 +50,51 @@ namespace TheDashlorisX
 	private readonly PictureBox S2Container1 = new PictureBox();
 	private readonly PictureBox S2Container2 = new PictureBox();
 
-	private readonly TextBox S2TextBox1 = new TextBox();
-
 	private readonly Label S2Label1 = new Label();
 
-	private void Init2()
+	private void InitS2Con(DashWindow DashWindow)
 	{
 	    try
 	    {
+		var Container1Size = new Size(S1Container1.Width, 115);
+		var Container1Loca = new Point(0, 0);
 
+		Control.Image(S1Container1, S2Container1, Container1Size, Container1Loca, S1Container1.BackColor);
+
+		var Container2Size = new Size(Container1Size.Width, Container1Size.Height - 30);
+		var Container2Loca = new Point(0, 30);
+		var Container2BCol = DashWindow.MenuBar.MenuBar.BackColor; 
+
+		Control.Image(S2Container1, S2Container2, Container2Size, Container2Loca, Container2BCol);
+
+		var LabelLoca = new Point(10, 0);
+
+		Control.Label(S2Container1, S2Label1, Size.Empty, LabelLoca, S2Container1.BackColor, Color.White, 1, 15, ("NS-Lookup"));
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+	private readonly TextBox S2TextBox1 = new TextBox();
+
+	private void Init2(PictureBox Capsule, DashWindow DashWindow)
+	{
+	    try
+	    {
+		InitS2Con(DashWindow);
+
+		var TextBoxSize = new Size(S2Container2.Width - 20, S2Container2.Height - 20);
+		var TextBoxLoca = new Point(10, 10);
+
+		Control.TextBox(S2Container2, S2TextBox1, TextBoxSize, TextBoxLoca, S2Container2.BackColor, Color.White, 1, 7, ReadOnly:true, Multiline:true, ScrollBar:true, FixedSize:false);
+
+		S2TextBox1.Text = string.Format
+		(
+		    "[!]: This functionality is experimental!"
+		);
 	    }
 
 	    catch (Exception E)
@@ -70,15 +107,84 @@ namespace TheDashlorisX
 	private readonly PictureBox S3Container1 = new PictureBox();
 	private readonly PictureBox S3Container2 = new PictureBox();
 
-	private readonly TextBox S3TextBox1 = new TextBox();
-
 	private readonly Label S3Label1 = new Label();
 
-	private void Init3()
+	private void InitS3Con(DashWindow DashWindow)
 	{
 	    try
 	    {
+		var Container1Size = new Size(S1Container1.Width, 115);
+		var Container1Loca = new Point(0, S2Container1.Top + S2Container1.Height + 20);
 
+		Control.Image(S1Container1, S3Container1, Container1Size, Container1Loca, S1Container1.BackColor);
+
+		var Container2Size = new Size(Container1Size.Width, Container1Size.Height - 30);
+		var Container2Loca = new Point(0, 30);
+		var Container2BCol = DashWindow.MenuBar.MenuBar.BackColor;
+
+		Control.Image(S3Container1, S3Container2, Container2Size, Container2Loca, Container2BCol);
+
+		var LabelLoca = new Point(10, 0);
+
+		Control.Label(S3Container1, S3Label1, Size.Empty, LabelLoca, S3Container1.BackColor, Color.White, 1, 15, ("DNS Lookup"));
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+	private readonly TextBox S3TextBox1 = new TextBox();
+
+	private void Init3(DashWindow DashWindow)
+	{
+	    try
+	    {
+		InitS3Con(DashWindow);
+
+		var TextBoxSize = new Size(S3Container2.Width - 20, S3Container2.Height - 20);
+		var TextBoxLoca = new Point(10, 10);
+
+		Control.TextBox(S3Container2, S3TextBox1, TextBoxSize, TextBoxLoca, S3Container2.BackColor, Color.White, 1, 7, ReadOnly: true, Multiline: true, ScrollBar: true, FixedSize: false);
+
+	    	S3TextBox1.Text = string.Format
+		(
+		    "[!]: This functionality is experimental!"
+		);
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+
+	private void RefreshIPData(InitThaDashlorisX Parent)
+	{
+	    try
+	    {
+		using (Process proc = new Process())
+		{
+		    proc.StartInfo = new ProcessStartInfo()
+		    {
+			UseShellExecute = false,
+
+			RedirectStandardOutput = true,
+			RedirectStandardError = true,
+
+			Arguments = "8.8.8.8",
+			FileName = $"C:\\Windows\\System32\\nslookup.exe"
+		    };
+
+		    proc.OutputDataReceived += (s, e) =>
+			S2TextBox1.AppendText(e.Data);
+
+		    proc.Start();
+
+		    proc.WaitForExit();
+		}
 	    }
 
 	    catch (Exception E)
@@ -97,13 +203,19 @@ namespace TheDashlorisX
 		if (needInit)
 		{
 		    Init1(Capsule, DashWindow);
-		    Init2();
-		    Init3();
+		    Init2(Capsule, DashWindow);
+		    Init3(DashWindow);
+
+		    Tool.Round(S2Container2, 6);
+		    Tool.Round(S3Container2, 6);
 
 		    needInit = false;
 		}
 
+		RefreshIPData(Parent);
+
 		Parent.HideContainers();
+
 		S1Container1.Show();
 	    }
 
