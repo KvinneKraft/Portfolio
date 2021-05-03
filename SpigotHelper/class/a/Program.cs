8d6c39f084122e795033e6ad452cbbe062783fc0
KvinneKraft <KvinneKraft@protonmail.com>
2021-05-03 22:36:41 +0300

    many changes, first push

---
+++ SpigotHelper/class/a/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+using DashFramework.Interface.Controls;
+using DashFramework.Interface.Tools;
+using DashFramework.Erroring;
+using DashFramework.Dialog;
+
+namespace SpigotHelper
+{
+    // Stone / Oak themed buttons
+    // Minecraft Font Style
+
+    public class SectorInitor
+    {
+	readonly DashControls Control = new DashControls();
+	readonly DashTools Tool = new DashTools();
+
+
+	readonly PictureBox S1Container = new PictureBox();
+
+	readonly Button S1Button1 = new Button();
+	readonly Button S1Button2 = new Button();
+	readonly Button S1Button3 = new Button();
+	readonly Button S1Button4 = new Button();
+
+	public void InitSector1(DashWindow App, PictureBox Main)
+	{
+	    try
+	    {
+		var ContainerImag = Properties.Resources.containerWallpaper as Image;
+		var ContainerSize = new Size(250, 50);
+		var ContainerLoca = new Point(-2, 15);
+
+		Control.Image(Main, S1Container, ContainerSize, ContainerLoca, Color.White, ContainerImag);
+
+		var ButtonSize = new Size(120, 20);
+		var ButtonBCol = App.MenuBar.MenuBar.BackColor;
+		var ButtonFCol = Color.White;
+
+		var Button1Loca = new Point(0, 0);
+		var Button2Loca = new Point(130, 0);
+		var Button3Loca = new Point(0, 30);
+		var Button4Loca = new Point(130, 30);
+
+		Control.Button(S1Container, S1Button1, ButtonSize, Button1Loca, ButtonBCol, ButtonFCol, 1, 7, ("Start Server"));
+		Control.Button(S1Container, S1Button2, ButtonSize, Button2Loca, ButtonBCol, ButtonFCol, 1, 7, ("Config Server"));
+		Control.Button(S1Container, S1Button3, ButtonSize, Button3Loca, ButtonBCol, ButtonFCol, 1, 7, ("Update Plugins"));
+		Control.Button(S1Container, S1Button4, ButtonSize, Button4Loca, ButtonBCol, ButtonFCol, 1, 7, ("API Downloads"));
+
+		foreach (Button Button in S1Container.Controls)
+		{
+		    Tool.Round(Button, 6);
+		}
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+
+	readonly PictureBox S2Container1 = new PictureBox();
+	readonly PictureBox S2Container2 = new PictureBox();
+
+	readonly TextBox S2TextBox = new TextBox();
+
+	readonly Button S2Button = new Button();
+
+	readonly Label S2Label1 = new Label();
+	readonly Label S2Label2 = new Label();
+	readonly Label S2Label3 = new Label();
+
+	public void InitSector2(DashWindow App, PictureBox Main)
+	{
+	    try
+	    {
+
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+    }
+
+    static class Program
+    { 
+	static readonly PictureBox DashWindowContainer = new PictureBox();
+	static readonly DashWindow DashWindow = new DashWindow();
+
+	static readonly SectorInitor DashInit = new SectorInitor();
+
+	static readonly DashControls Control = new DashControls();
+	static readonly DashTools Tool = new DashTools();
+
+	static DashWindow GetDashWindow()
+	{
+	    try
+	    {
+		var AppMCol = Color.FromArgb(153, 54, 54);//(132, 66, 245);
+		var AppBCol = Color.FromArgb(54, 160, 255);
+		var AppSize = new Size(300, 350);
+
+		DashWindow.InitializeWindow(AppSize, ("Spigot Helper"), AppBCol, AppMCol, CloseHideApp: false);
+
+		DashWindow.MenuBar.LogoLayer2.BackgroundImage = Properties.Resources.appWallpaper;
+		DashWindow.BackgroundImage = Properties.Resources.appWallpaper;
+
+		var ContainerSize = new Size(280, 330 - DashWindow.MenuBar.MenuBar.Height);
+		var ContainerImag = Properties.Resources.containerWallpaper as Image;
+		var ContainerLoca = new Point(-2, 36); 
+
+		Control.Image(DashWindow, DashWindowContainer, ContainerSize, ContainerLoca, AppBCol, ContainerImag);
+
+		Tool.Round(DashWindowContainer, 8);
+
+		DashInit.InitSector1(DashWindow, DashWindowContainer);
+		DashInit.InitSector2(DashWindow, DashWindowContainer);
+
+		return DashWindow;
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+	[STAThread]
+	static void Main()
+	{
+	    try
+	    {
+		Application.EnableVisualStyles();
+		Application.SetCompatibleTextRenderingDefault(false);
+		Application.Run(GetDashWindow());
+	    }
+
+	    catch (Exception E)
+	    {
+		ErrorHandler.JustDoIt(E);
+	    }
+        }
+    }
+}
