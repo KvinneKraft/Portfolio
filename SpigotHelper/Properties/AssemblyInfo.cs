ac61cccc94536ca79f777f3355ddd094c02ec1f7
KvinneKraft <KvinneKraft@protonmail.com>
2021-05-04 19:26:42 +0300

    Update AssemblyInfo.cs

--- SpigotHelper/Properties/AssemblyInfo.cs
+++ SpigotHelper/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 
 
 [assembly: AssemblyTitle("SpigotHelper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Efficiency Upgrade")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Dashies Softwaries")]
 [assembly: AssemblyProduct("SpigotHelper")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © KvinneKraft 2021")]
+[assembly: AssemblyTrademark("KvinneKraft")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("32c48c53-257f-4d83-9ec6-c9ac7407f361")]
