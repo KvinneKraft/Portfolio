0b3181fc5d4fe63c1df646507feeccb9c14a319b
KvinneKraft <KvinneKraft@protonmail.com>
2021-05-11 19:20:48 +0300

    Update Resources.Designer.cs

--- TheDashlorisX/properties/Resources.Designer.cs
+++ TheDashlorisX/properties/Resources.Designer.cs
@@ -109,5 +109,15 @@ namespace TheDashlorisX.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Splash {
+            get {
+                object obj = ResourceManager.GetObject("Splash", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
