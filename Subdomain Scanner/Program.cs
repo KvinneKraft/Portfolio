9d5c230bc3ba22879a0b12e9de95621743838744
KvinneKraft <KvinneKraft@protonmail.com>
2021-05-12 23:48:28 +0300

    hooked startup with DashApp

--- Subdomain Scanner/Program.cs
+++ Subdomain Scanner/Program.cs
@@ -1,22 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Subdomain_Scanner
+namespace SubdomainScanner
 {
     static class Program
     {
-	/// <summary>
-	/// The main entry point for the application.
-	/// </summary>
-	[STAThread]
-	static void Main()
+	[STAThread] static void Main()
 	{
 	    Application.EnableVisualStyles();
 	    Application.SetCompatibleTextRenderingDefault(false);
-	    Application.Run(new MainGUI());
+
+	    MainGUI DashApp = new MainGUI();
+	    
+	    Application.Run(DashApp.DashApp);
+	    Environment.Exit(-1);
 	}
     }
 }
