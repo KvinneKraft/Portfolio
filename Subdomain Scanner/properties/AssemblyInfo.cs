a6eeb68034c07abcf52004146ae1bc5e6fbb8505
KvinneKraft <KvinneKraft@protonmail.com>
2021-05-12 22:59:38 +0300

    setup assembly file

---
+++ Subdomain Scanner/properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Subdomain Scanner")]
+[assembly: AssemblyDescription("Find what you are looking for.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Dashies Softwaries")]
+[assembly: AssemblyProduct("Subdomain Scanner")]
+[assembly: AssemblyCopyright("Copyright © KvinneKraft 2021")]
+[assembly: AssemblyTrademark("Dashies Softwaries")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("91c9cc4d-91bd-4c2c-b43b-ffe958b3106c")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
