0e6f8b859d5b8295ab9f6ae1cf4db785a0c53b1e
KvinneKraft <KvinneKraft@protonmail.com>
2021-05-24 17:48:30 +0300

    setup DashWindow

---
+++ PugPawz Helper Panel/class/a/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+using DashFramework.Erroring;
+using DashFramework.Dialog;
+
+namespace DashApplication
+{
+    static class Program
+    {
+	readonly static MainGUI mainGUI = new MainGUI();
+
+	[STAThread]
+	static void Main()
+	{
+	    Application.EnableVisualStyles();
+	    Application.SetCompatibleTextRenderingDefault(false);
+
+	    try
+	    {
+		using (DashWindow dashWindow = new DashWindow())
+		{
+		    Size AppSize = new Size(350, 350);
+
+		    string AppTitle = ("Dash Application");
+
+		    Color AppMCol = Color.CornflowerBlue;
+		    Color AppBCol = Color.White;
+
+		    dashWindow.InitializeWindow(AppSize, AppTitle, AppBCol, AppMCol, CloseHideApp: false);
+
+		    dashWindow.MenuBar.Button1.Click += (s, e) =>
+			Environment.Exit(-1);
+		    dashWindow.FormClosing += (s, e) =>
+			Environment.Exit(-1);
+
+		    mainGUI.Initiator(dashWindow);
+
+		    Application.Run(dashWindow);
+		}
+	    }
+
+	    catch (Exception E)
+	    {
+		ErrorHandler.JustDoIt(E);
+	    }
+	}
+    }
+}
