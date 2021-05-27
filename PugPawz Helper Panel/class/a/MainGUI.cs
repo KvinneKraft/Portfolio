f8599df1ca8ef423f78694392b7c0a5305903775
KvinneKraft <KvinneKraft@protonmail.com>
2021-05-27 15:25:27 +0300

    added mainframe + bottombar

--- PugPawz Helper Panel/class/a/MainGUI.cs
+++ PugPawz Helper Panel/class/a/MainGUI.cs
@@ -19,13 +19,65 @@ using DashFramework.Dialog;
 
 namespace DashApplication
 {
-    public partial class MainGUI
+    public class MainGUI
     {
+	DashControls Controls = new DashControls();
+	DashTools Tools = new DashTools();
+
+	DashWindow App = null;
+
+
+	readonly PictureBox ContA1 = new PictureBox();
+
+	void Init1()
+	{
+	    try
+	    {
+		var Cont1Size = new Size(App.Width, 38);
+		var Cont1Loca = new Point(0, App.Bottom - 38);
+		var Cont1BCol = App.values.getBarColor();
+
+		Controls.Image(App, ContA1, Cont1Size, Cont1Loca, Cont1BCol);
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+
+	readonly PictureBox ContB1 = new PictureBox();
+
+	void Init2()
+	{
+	    try
+	    {
+		int MainFrameHeight = App.Height - App.values.Height() - ContA1.Height; // Recode MenuBar patterns and add methods for obtainable values.
+		int MainFrameWidth = App.Width - 4;
+
+		var Cont1Size = new Size(MainFrameWidth, MainFrameHeight);
+		var Cont1Loca = new Point(2, App.values.Height());
+		var Cont1BCol = App.BackColor;
+
+		Controls.Image(App, ContB1, Cont1Size, Cont1Loca, Cont1BCol);
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+
+
 	public void Initiator(DashWindow inst)
 	{
 	    try
 	    {
-		// Process Code
+		App = inst;
+
+		Init1();
+		Init2();
 	    }
 
 	    catch (Exception E)
