9426d68f855f634305fbc4a482abdb5e57c71932
KvinneKraft <KvinneKraft@protonmail.com>
2021-05-29 02:44:03 +0300

    Code Skeleton for main frame + pages

---
+++ PugPawzPanel/class/c/Init2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+using DashFramework.Interface.Controls;
+using DashFramework.Interface.Tools;
+
+using DashFramework.Erroring;
+using DashFramework.Dialog;
+
+namespace DashApplication
+{
+    public class Init2
+    {
+	readonly DashControls Controls = new DashControls();
+	readonly DashTools Tools = new DashTools();
+
+
+	public void SecondaryInitiate(DashWindow app, PictureBox frame)
+	{
+	    try
+	    {
+		// Main Frame Functionality + Pages and all shit
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+    }
+}
