120413f7b9cdeb2ddb695d1e68ea76fecb4756c3
KvinneKraft <KvinneKraft@protonmail.com>
2021-05-29 02:43:44 +0300

    added code skeleton for MenuBar initialization

---
+++ PugPawzPanel/class/c/Init1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+using DashFramework.Interface.Controls;
+using DashFramework.Interface.Tools;
+
+using DashFramework.Erroring;
+using DashFramework.Dialog;
+
+namespace DashApplication
+{
+    public class Init1
+    {
+	readonly DashControls Controls = new DashControls();
+	readonly DashTools Tools = new DashTools();
+
+
+	public void SecondaryInitiate(DashWindow app, PictureBox frame)
+	{
+	    try
+	    {
+		// Bottom Menu Bar Functionality
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+    }
+}
