9437614b4501841428b2d8f9b71e61c8f5a2206d
KvinneKraft <KvinneKraft@protonmail.com>
2021-06-01 22:54:34 +0300

    creation of class file PageC

---
+++ PugPawzPanel/class/d/InsertProduct.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+using DashFramework.Interface.Controls;
+using DashFramework.Interface.Tools;
+
+using DashFramework.Erroring;
+using DashFramework.Dialog;
+
+namespace DashApplication
+{
+    public class PageC : Expansion
+    {
+	public PageC(DashWindow app, PictureBox frame)
+	{
+	    try
+	    {
+		RenderDefaultLayout(app, frame);
+
+
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+    }
+}
