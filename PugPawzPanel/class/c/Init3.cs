74cd2db0fa1aec4fe592904ad83927a9746d9f1f
KvinneKraft <KvinneKraft@protonmail.com>
2021-06-01 23:35:07 +0300

    added skeleton for  configurastion handler

---
+++ PugPawzPanel/class/c/Init3.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+using DashFramework.Interface.Controls;
+using DashFramework.Runnables;
+using DashFramework.Erroring;
+using DashFramework.Dialog;
+
+namespace DashApplication
+{
+    public class Init3
+    {
+	readonly string[] defaultSettingsFormat = new string[]
+	{//Default Configuration Layout right here.
+	    "",
+	    "",
+	    "",
+	    "",
+	    "",
+	    "",
+	    "",
+	    "",
+	    ""
+	};
+
+	//Get and Set methods for config here.
+
+	public void SecondaryInitiate(DashWindow app, PictureBox frame)
+	{
+	    try
+	    {
+		//Create missing config directory
+		//Create missing config file
+
+		Runnable runnable = new Runnable();
+
+		runnable.RunTaskLater
+		(
+		    app, 
+		    
+		    () => 
+		    {
+			MessageBox.Show("Test");
+			//Load Configuration
+			//Set values to temp variables
+			//Update main values with temp variables every 500ms
+		    }, 
+		    
+		    8000, true
+		);
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+    }
+}
