1695e85364b9fb55b603de5bd258fe6d9dbf4b92
KvinneKraft <KvinneKraft@protonmail.com>
2021-06-04 20:03:23 +0300

    Create AssemblyInfo.cs

---
+++ GateHey/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PugPawz_Helper_Panel")]
+[assembly: AssemblyDescription("PugPawz_Helper_Panel")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Dashies Softwaries")]
+[assembly: AssemblyProduct("PugPawz_Helper_Panel")]
+[assembly: AssemblyCopyright("Copyright © KvinneKraft 2021")]
+[assembly: AssemblyTrademark("Dashies Softwaries")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("84f7deef-0a23-4967-ae5a-a181365eff7f")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
