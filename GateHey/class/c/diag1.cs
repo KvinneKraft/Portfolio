76809e3b6b9b4a3a321c01a7315a05c4e8da3ea2
KvinneKraft <KvinneKraft@protonmail.com>
2021-06-09 19:06:59 +0300

    setup code initiation skeleton

--- GateHey/class/c/diag1.cs
+++ GateHey/class/c/diag1.cs
@@ -1,6 +1,6 @@
 // Author: Dashie
 // Version: 1.0
-
+// Port Selector
 
 using System;
 using System.IO;
@@ -22,5 +22,90 @@ using DashFramework.Dialog;
 
 namespace GateHey
 {
+    public class Dialog1
+    {
+	class InitiateTop
+	{
+	    readonly DashControls Controls = new DashControls();
+	    readonly DashTools Tools = new DashTools();
 
+
+	    public void Initiate(DashWindow Parent, DashWindow Inst)
+	    {
+		try
+		{
+		    // Top Section
+		}
+
+		catch (Exception E)
+		{
+		    throw (ErrorHandler.GetException(E));
+		}
+	    }
+	}
+
+
+	class InitiateBottom
+	{
+	    readonly DashControls Controls = new DashControls();
+	    readonly DashTools Tools = new DashTools();
+
+
+	    public void Initiate(DashWindow Parent, DashWindow Inst)
+	    {
+		try
+		{
+		    // Bottom Section
+		}
+
+		catch (Exception E)
+		{
+		    throw (ErrorHandler.GetException(E));
+		}
+	    }
+	}
+
+
+	class InitiateMiddle
+	{
+	    readonly DashControls Controls = new DashControls();
+	    readonly DashTools Tools = new DashTools();
+
+
+	    public void Initiate(DashWindow Parent, DashWindow Inst)
+	    {
+		try
+		{
+		    // Middle Section
+		}
+
+		catch (Exception E)
+		{
+		    throw (ErrorHandler.GetException(E));
+		}
+	    }
+	}
+
+
+	readonly InitiateBottom InitiateB = new InitiateBottom();
+	readonly InitiateMiddle InitiateM = new InitiateMiddle();
+	readonly InitiateTop InitiateT = new InitiateTop();
+
+	readonly DashWindow Parent = new DashWindow();
+
+	public void Initiator(DashWindow inst)
+	{
+	    try
+	    {
+		InitiateT.Initiate(inst, Parent);
+		InitiateB.Initiate(inst, Parent);
+		InitiateM.Initiate(inst, Parent);
+	    }
+
+	    catch (Exception E)
+	    {
+		throw (ErrorHandler.GetException(E));
+	    }
+	}
+    }
 }
