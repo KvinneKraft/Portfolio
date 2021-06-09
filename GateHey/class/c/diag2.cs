4f20a06f719f11e505dd1d5248b9f4931f509ba1
KvinneKraft <KvinneKraft@protonmail.com>
2021-06-09 18:58:20 +0300

    created separate dialog classes

---
+++ GateHey/class/c/diag2.cs
@@ -0,0 +1,26 @@
+// Author: Dashie
+// Version: 1.0
+
+
+using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Drawing;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+using DashFramework.Interface.Controls;
+using DashFramework.Interface.Tools;
+
+using DashFramework.Erroring;
+using DashFramework.Dialog;
+
+
+namespace GateHey
+{
+
+}
