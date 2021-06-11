4995f22ddb24ac07b962b3b140e06da010c96767
KvinneKraft <KvinneKraft@protonmail.com>
2021-06-11 20:13:59 +0300

    added static ports list

---
+++ GateHey/class/c/vars.cs
@@ -0,0 +1,29 @@
+﻿// Author: Dashie
+// Version: 1.0
+// Port Selector
+
+using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Drawing;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+using DashFramework.Interface.Controls;
+using DashFramework.Interface.Tools;
+
+using DashFramework.Erroring;
+using DashFramework.Dialog;
+
+
+namespace GateHey
+{
+    public class Var
+    {
+	public static List<int> Ports = new List<int>();
+    }
+}
