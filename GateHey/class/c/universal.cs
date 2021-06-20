31748b61de411be90d43fec240c3f1c1b647ee50
KvinneKraft <KvinneKraft@protonmail.com>
2021-06-20 18:57:00 +0300

    added universal msgbox method for universal portsettingscorrect method

--- GateHey/class/c/universal.cs
+++ GateHey/class/c/universal.cs
@@ -23,9 +23,142 @@ using DashFramework.Dialog;
 
 namespace GateHey
 {
-    public class Universal
+    public class Universal : DashTools
     {
+	readonly static DashTools Tools = new DashTools();
 	readonly static DashNet DaNet = new DashNet();
+
+
+	public static void MsgBox(string msg, bool toggle)
+	{
+	    if (toggle)
+	    {
+		Tools.MsgBox(msg, "Port Selector", icon: MessageBoxIcon.Warning);
+	    }
+	}
+
+
+	public static int scanType = 1;//1=single,2=multi,3=ranged
+
+	public static bool PortSettingsCorrect(string data, bool toggle)
+	{
+	    try
+	    {
+		bool IsPort(string slice)
+		{
+		    try
+		    {
+			int port = int.Parse(slice);
+			return (port > 0 && port <= 65535);
+		    }
+
+		    catch
+		    {
+			return false;
+		    }
+		}
+
+		bool ArePortsOkay(string[] portData)
+		{
+		    foreach (string portSlice in portData)
+		    {
+			if (!IsPort(portSlice))
+			{
+			    return false;
+			}
+		    }
+
+		    return true;
+		}
+
+		List<int> ports = new List<int>();
+
+		string invalidMsg = ("The port selection found within the file seems incorrect.  Please make sure you use the correct format, else errors may occur.");
+
+		if (data.Length < 1)
+		{
+		    MsgBox(invalidMsg, toggle);
+		    return false;
+		}
+
+		else if (data.Contains("-"))
+		{
+		    string[] portData = data.Split('-');
+
+		    if (portData.Length != 2)
+		    {
+			MsgBox(invalidMsg, toggle);
+			return false;
+		    }
+
+		    else if (!ArePortsOkay(portData))
+		    {
+			MsgBox(invalidMsg, toggle);
+			return false;
+		    }
+
+		    int min = int.Parse(portData[0]);
+		    int max = int.Parse(portData[1]);
+
+		    if (min >= max)
+		    {
+			MsgBox(invalidMsg, toggle);
+			return false;
+		    }
+
+		    ports.Add(min);
+		    ports.Add(max);
+
+		    scanType = 3;
+		}
+
+		else if (data.Contains(","))
+		{
+		    string[] portData = data.Split(',');
+
+		    if (portData.Length < 1)
+		    {
+			MsgBox(invalidMsg, toggle);
+			return false;
+		    }
+
+		    else if (!ArePortsOkay(portData))
+		    {
+			MsgBox(invalidMsg, toggle);
+			return false;
+		    }
+
+		    foreach (string dataSlice in portData)
+		    {
+			ports.Add(int.Parse(dataSlice));
+		    }
+
+		    scanType = 2;
+		}
+
+		else
+		{
+		    if (!IsPort(data))
+		    {
+			MsgBox(invalidMsg, toggle);
+			return false;
+		    }
+
+		    ports.Add(int.Parse(data));
+		    scanType = 1;
+		}
+
+		Ports.Clear();
+		Ports.AddRange(ports);
+
+		return true;
+	    }
+
+	    catch
+	    {
+		return false;
+	    }
+	}
 
 
 	public static string LastError = string.Empty;
@@ -42,11 +175,17 @@ namespace GateHey
 	}
 
 
-	public static bool SettingsValidation(MainGUI.Initiator2 MainSettings)
+	public static bool SettingsValidation(MainGUI.Initiator2 MainSettings, bool Toggle = true)
 	{
 	    try
 	    {
 		var ComponentValues = MainSettings.GetComponentValues();
+
+		if (!PortSettingsCorrect(MainSettings.Dialog1.InitiateM.TxtBox.Text, Toggle))
+		{
+		    SetError("invalid port settings");
+		    return false;
+		}
 
 		if (!DaNet.CanIP(ComponentValues["host"]))
 		{
