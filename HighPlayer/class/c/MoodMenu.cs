f7db2ef62f6fbccf798531b4968ae82d7683adc2
KvinneKraft <KvinneKraft@protonmail.com>
2021-07-07 20:55:20 +0300

    finished mood menu dialog

--- HighPlayer/class/c/MoodMenu.cs
+++ HighPlayer/class/c/MoodMenu.cs
@@ -47,15 +47,53 @@ namespace HighPlayer
 
         public class Init2
         {
-	    readonly DashPanel Panel1 = new DashPanel();//main surrounding container
-	    readonly DashPanel Panel2 = new DashPanel();//inner textbox container
-	    readonly DashPanel Panel3 = new DashPanel();//actual textbox container
-
-	    readonly Label Label1 = new Label();
+	    readonly DashPanel Panel1 = new DashPanel();
+	    readonly DashPanel Panel2 = new DashPanel();
+	    readonly DashPanel Panel3 = new DashPanel();
+	    
+	    readonly TextBox TextBox = new TextBox();
+	    readonly Label Label = new Label();
 
 	    public void Initiate(DashWindow Inst)
 	    {
-		// Size of bottom bar will be 30 
+		Tools.SortCode(("Base Containers"), () =>
+		{
+		    Size Panel1Size = new Size(Parent.Width - 20, Parent.Height - 80);
+		    Size Panel2Size = new Size(Panel1Size.Width, Panel1Size.Height - 24);
+		    Size Panel3Size = new Size(Panel2Size.Width - 16, Panel2Size.Height - 16);
+		    Point Panel1Loca = new Point(-2, 35);
+		    Point Panel2Loca = new Point(0, 24);
+		    Point Panel3Loca = new Point(8, 8);
+		    Color Panel1BCol = Parent.BackColor;
+		    Color Panel2BCol = Color.FromArgb(10, 45, 59);
+		    
+		    Controls.Panel(Parent, Panel1, Panel1Size, Panel1Loca, Panel1BCol);
+		    Controls.Panel(Panel1, Panel2, Panel2Size, Panel2Loca, Panel2BCol);
+		    Controls.Panel(Panel2, Panel3, Panel3Size, Panel3Loca, Panel2BCol);
+		});
+
+		Tools.SortCode(("Title Injection"), () =>
+		{
+		    Point LabelLoca = new Point(0, 0);
+		    Color LabelBCol = Panel1.BackColor;
+		    Color LabelFCol = Color.White;
+
+		    Controls.Label(Panel1, Label, Size.Empty, LabelLoca, LabelBCol, 
+			LabelFCol, ("Loaded Moods"), FontSize: 13);
+		});
+
+		Tools.SortCode(("Mod Container"), () =>
+		{
+		    Size TextBoxSize = Panel3.Size;
+		    Point TextBoxLoca = new Point(0, 0);
+		    Color TextBoxBCol = Panel3.BackColor;
+		    Color TextBoxFCol = Color.White;
+
+		    Controls.TextBox(Panel3, TextBox, TextBoxSize, TextBoxLoca, TextBoxBCol, 
+			TextBoxFCol, 1, 12, Multiline: true, FixedSize: false);
+		    
+		    Tools.Round(Panel2, 6);
+		});
 	    }
 	}
 
@@ -71,7 +109,37 @@ namespace HighPlayer
 
 	    public void Initiate(DashWindow Inst, MainGUI Origin)
 	    {
-		// Size of bar will be 30 and the buttons will be 24
+		Tools.SortCode(("Containers"), () => 
+		{
+		    Size Panel1Size = new Size(Parent.Width, 34);
+		    Size Panel2Size = new Size(270, 22);
+		    Point Panel1Loca = new Point(0, Parent.Height - 34);
+		    Point Panel2Loca = new Point(-2, -2);
+		    Color Panel1BCol = Parent.values.getBarColor();
+
+		    Controls.Panel(Parent, Panel1, Panel1Size, Panel1Loca, Panel1BCol);
+		    Controls.Panel(Panel1, Panel2, Panel2Size, Panel2Loca, Panel1BCol);
+		});
+
+		Tools.SortCode(("Buttons"), () =>
+		{
+		    Quickify quicky = new Quickify()
+		    {
+			BttnBCol = Panel1.BackColor,
+			BttnSize = new Size(75, 22),
+			BttnParent = Panel2,
+			BttnBorder = true,
+			BttnFpts = 12,
+		    };
+
+		    Point Button3Loca = new Point(195, 0);
+		    Point Button2Loca = new Point(-2, 0);
+		    Point Button1Loca = new Point(0, 0);
+
+		    quicky.QuickButton(Button1, ("New"), Button1Loca);
+		    quicky.QuickButton(Button2, ("Save"), Button2Loca);
+		    quicky.QuickButton(Button3, ("Refresh"), Button3Loca);
+		});
 	    }
 	}
 
